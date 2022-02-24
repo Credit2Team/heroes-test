d8f76f8740c6f5e2cf218812cd1b395b0abb4a0c
BrickyTrout <a.wong45789@gmail.com>
2022-02-24 12:43:26 +1100

    hero name filtered by e

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService
+      .getHeroes()
+      .pipe(map((heroes) => this.filterHeroByE(heroes)))
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
+
+  private filterHeroByE(heros: Hero[]): Hero[] {
+    return heros.filter((hero) => hero.name.toLowerCase().includes('e'));
   }
 }
